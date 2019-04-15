f9348fe6d7d82d656af3174262f025dc2f967974
Mariam <mariam.a.qasim@gmail.com>
2019-04-15 13:02:53 -0400

    movie-details is moved to movies now

--- src/app/movies/movie-details/movie-details.page.ts
+++ src/app/movies/movie-details/movie-details.page.ts
@@ -1,4 +1,4 @@
-import { MovieService } from '../services/movie.service';
+import { MovieService } from '../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
